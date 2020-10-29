379df81d02c4e9acc0489e7f7f2e5f598a0b4dee
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-10-30 00:45:38 +0100

    prepare for pip

--- setup.py
+++ setup.py
@@ -6,5 +6,14 @@ setup(
     packages=['embit',],
     license='MIT license',
     url='https://github.com/diybitcoinhardware/embit',
+    description = 'yet another bitcoin library',
     long_description="A minimal bitcoin library for MicroPython and Python3 with a focus on embedded systems.",
+    author = 'Stepan Snigirev',
+    author_email = 'snigirev.stepan@gmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Flask",
+    ],
 )
