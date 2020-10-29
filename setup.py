19072cd43fe63625db78d7a9d4b666076a9565db
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-10-30 00:46:22 +0100

    remove flask

--- setup.py
+++ setup.py
@@ -14,6 +14,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Framework :: Flask",
     ],
 )
