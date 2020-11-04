7c896e7bee0f6382abeffa4464eda5fb084e81c1
Stepan Snigirev <snigirev.stepan@gmail.com>
2020-11-04 11:01:05 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='embit',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT license',
     url='https://github.com/diybitcoinhardware/embit',
     description = 'yet another bitcoin library',
