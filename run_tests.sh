0e4f26c963720baa33e1ef6762515035e9a38666
alvroble <50918598+alvroble@users.noreply.github.com>
2025-07-25 18:39:16 +0200

    secp256k1 v0.7.0

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$BUILD_SECP256K1_FROM_SOURCE" = "1" ]; then
     echo "Cloning and building secp256k1 from https://github.com/bitcoin-core/secp256k1 ..."
-    git clone --branch v0.6.0 --depth 1 https://github.com/bitcoin-core/secp256k1.git /tmp/secp256k1
+    git clone --branch v0.7.0 --depth 1 https://github.com/bitcoin-core/secp256k1.git /tmp/secp256k1
     cd /tmp/secp256k1
     ./autogen.sh
     ./configure --enable-module-ecdh --enable-module-recovery --enable-module-schnorrsig 
