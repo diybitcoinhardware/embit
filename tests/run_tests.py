14e64d07ef8dc618e85e30a8c4b90e2db25d4a2f
Mike Tolkachev <contact@miketolkachev.dev>
2026-03-17 18:05:10 -0300

    tests: fix path to local sources

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,7 +1,8 @@
 import sys
 
 if sys.implementation.name == "micropython":
-    sys.path.append("../src")
+    # Ensure local sources take precedence over frozen/std lib packages.
+    sys.path.insert(0, "../src")
 import unittest
 
 if __name__ == "__main__":
